fb8523742b0c9c6200b27f7f7dfdfb2d1b0ff849
Khronos2k <silver_spear20@hotmail.com>
2023-09-11 19:47:12 -0300

    add: styles

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
+//  *Expres: require - (not modify)
+const express = require('express');
+const app = express();
+
+//--
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 //  **Routes: require routes
 const indexRouter = require('./routes/indexRoute');
 const productRouter = require('./routes/productRoute');
 const userRouter = require('./routes/userRoutes');
-//  *Expres: require - (not modify)
-const express = require('express');
-const app = express();
 //  *Path: require - (not modify)
 const path = require('path');
 
 //  *Express: ubicación de los archivos de imagenes & estilos - (not modify)
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
+
+//--
+app.use(express.urlencoded({extended:false}));
+app.use(express.json())
 
 //  *Express: indicamos la utilización de template engine y la ubicación de las vistas - (not modify)
 app.set('view engine', 'ejs')
