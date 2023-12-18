828299ceb45cdcbefac1f4fb9d22634f02eed1ff
SantiagoV0 <sxd292@gmail.com>
2023-12-18 20:39:51 -0300

    New routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const userRouter = express.Router();
 const userController = require('../controllers/userController');
 //  ****    Middlewares    ****
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
+// Rutas que requieren autenticación
+userRouter.get('/profile/:id', isAuthenticated, userController.showProfile);
 //  ****    GET:USERS-LIST    ****
 userRouter.get('/list', userController.showList);
 //  ****    GET:USER-LOGIN    ****
@@ -12,13 +20,15 @@ userRouter.get('/profile/:id', userController.showProfile);
 
 //  ****    GET:USER-CREATE    ****
 userRouter.get('/register', userController.showCreate);
+userRouter.post('/register',userController.store);
+
 //  ****    POST:USER-STORE    ****
-userRouter.get('/', userController.store);
+userRouter.put('/', userController.store);
 
 //  ****    GET:USER-EDIT    ****
 userRouter.get('/update/:id', userController.showEdit);
 //  ****    PUT:USER-UPDATE    ****
-userRouter.get('/update/:id', userController.update);
+userRouter.put('/update/:id', userController.update);
 
 //  ****    DELETE    ****
 userRouter.get('/delete/:id', userController.delete);
